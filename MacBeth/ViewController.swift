9dcbe4474ce06b938ab468d144921d933409c67a
Denny Mathew <devdenmathew@gmail.com>
2020-04-18 13:18:56 +0800

    Initial Commit

---
+++ MacBeth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacBeth
+//
+//  Created by Denny Mathew on 18/04/20.
+//  Copyright © 2020 Densigns. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
